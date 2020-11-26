ce0c487863f009420754eea71d8b7f63dc141766
Puspi <sardanap95@gmail.com>
2020-11-26 23:17:15 +0530

    Finished up the app.

--- app.js
+++ app.js
@@ -10,26 +10,111 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+let oneMngr = 0;
+let employees = [];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+promptUser();
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+async function promptUser() {
+  try {
+    console.log("Welcome to the Software Engineering Team Generator");
+    const { name } = await inquirer.prompt({
+      type: "input",
+      name: "name",
+      message: "Employee's name:  ",
+    });
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+    const { id } = await inquirer.prompt({
+      type: "input",
+      name: "id",
+      message: "Employee's ID:  ",
+    });
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+    const { email } = await inquirer.prompt({
+      type: "input",
+      name: "email",
+      message: "Employee's email:  ",
+      default: () => {},
+      validate: function (email) {
+        return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+          String(email).toLowerCase()
+        )
+          ? true
+          : false;
+      },
+    });
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+    const { role } = await inquirer.prompt({
+      type: "list",
+      name: "role",
+      message: "Select which part of the team this employee is on:",
+      choices: ["Engineer", "Intern", "Manager"],
+    });
+
+    const { github } = await inquirer.prompt({
+      type: "input",
+      name: "github",
+      message: "Engineers will enter thier Github:",
+      when: (answers) => role === "Engineer",
+    });
+
+    const { school } = await inquirer.prompt({
+      type: "input",
+      name: "school",
+      message: "Please entrer your school name.",
+      when: (answers) => role === "Intern",
+    });
+
+    const { officeNumber } = await inquirer.prompt({
+      type: "input",
+      name: "officeNumber",
+      message: "Enter Manager's office number:",
+      when: (answers) => role === "Manager",
+    });
+
+    switch (role) {
+      case "Engineer":
+        employees.push(new Engineer(name, id, email, github));
+        console.log(name + " is part of the engineering team now!");
+        break;
+      case "Intern":
+        employees.push(new Intern(name, id, email, school));
+        console.log(name + " is part of intern team now!");
+        break;
+      case "Manager":
+        if (oneMngr < 1) {
+          employees.push(new Manager(name, id, email, officeNumber));
+          console.log(name + " is the team manager.");
+          oneMngr++;
+        } else {
+          console.log("There is only one manager for this project.");
+        }
+        break;
+      default:
+        console.log("Opps, wrong role");
+        break;
+    }
+
+    const { addTeamMember } = await inquirer.prompt({
+      type: "list",
+      message: "Add another Team Member?",
+      name: "addTeamMember",
+      choices: ["Yes", "No"],
+    });
+    let addMem = addTeamMember;
+    switch (addMem) {
+      case "Yes":
+        promptUser();
+        break;
+      case "No":
+        if (!fs.existsSync(OUTPUT_DIR)) {
+          fs.mkdirSync(OUTPUT_DIR);
+        }
+        fs.writeFileSync(outputPath, render(employees), "utf8");
+        console.log("This HR Team will be the Summer Success for the Pushpi Corps!");
+        break;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
